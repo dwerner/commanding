715e25e9296524e07b8a4e42b26bf03c639b0b86
danwerne@cisco.com <danwerne@cisco.com>
2013-10-28 11:35:54 -0700

    refactoring DSL

--- commanding/commands.py
+++ commanding/commands.py
@@ -9,96 +9,109 @@ class Message(object):
       print self.message
 
 
-class Command(object):
+class Run(object):
 
-   def __init__(self, cmd):
+   hasRun = False
+   verbose = False
+
+   def __init__(self, cmd, verbose=False):
       self.command = cmd
+      self.verbose = verbose
 
    def run(self):
-      Message(self.command).run()
+      if self.hasRun:
+         raise Exception("Run has already run! {}".format(self))
+
       self.process_options = {"shell":True, "stdout":PIPE, "stderr":PIPE}
       self.process = Popen([self.command], **self.process_options)
       self.stdout, self.stderr = self.process.communicate()
       self.process.wait()
+
+      if self.verbose:
+         Message(self.command).run()
+         Message("Value: {}".format(self.value()))
+
+      self.hasRun = True
       return self
 
-   def first(self):
+   def value(self):
+      if not self.hasRun:
+         self.run()
       return self.stdout.rstrip()
 
    def matches(self, match):
-      return match in self.run().first()
+      return match in self.value()
 
    def isnt(self, match):
-      return match in self.run().first()
+      return match in self.value()
 
    def __str__(self):
       return '"{}"'.format(self.command)
 
 
 
-class Where(Command):
+class Exists(Run):
    def __init__(self, test):
-      super(Where, self).__init__("which {}".format(test))
+      super(Exists, self).__init__("which {}".format(test))
 
 
-class Wget(Command):
+class Wget(Run):
    def __init__(self, url):
       self.command = "wget {}".format(url)
 
 
-class AptPPA(Command):
+class AptPPA(Run):
    def __init__(self, ppa):
       self.command = "sudo add-apt-repository -y {}".format(ppa)
 
 
-class Apt(Command):
+class Apt(Run):
    def __init__(self, pkg):
       self.command = "sudo apt-get install -y {}".format(pkg)
 
 
-class AptUpdate(Command):
+class AptUpdate(Run):
    def __init__(self):
       self.command = "sudo apt-get update"
 
 
-class Pip(Command):
+class Pip(Run):
    def __init__(self, pkg):
       self.command = "sudo pip install {}".format(pkg)
 
 
-class Unzip(Command):
+class Unzip(Run):
    def __init__(self, archive, dest):
       self.command = "unzip {} {}".format(archive, dest)
 
 
-class Brew(Command):
+class Brew(Run):
    def __init__(self, pkg):
       self.command = "sudo brew install {}".format(pkg)
 
+class When(object):
 
-class Task(object):
-   def __init__(self, *args):
-      self.tasks = args
+   commands = []
+   else_commands = []
 
-   def run(self):
-      print "Running task"
-      for task in self.tasks:
-         task.run()
-
-   def __str__(self):
-      return " -> ".join(["{}".format(s) for s in self.tasks])
-
-
-class Do(object):
-   def __init__(self, condition, command):
+   def __init__(self, condition):
       self.condition = condition
-      self.command = command
 
    def run(self):
       if self.condition:
-         self.command.run()
+         for cmd in self.commands:
+           cmd.run()
       else:
-         Message("Won't Do : {}".format(self.command)).run()
+         Message("Not executing {}".format(self.commands))
+
+   def do(self, *args):
+      self.commands.extend(args)
+      return self
+
+
+   def elseWhen(self, condition):
+      return When(condition)
 
    def __str__(self):
-      return "Conditional: {}".format(self.command)
+      return "condition:{} commands: {} else: {}".format(self.condition, self.commands, self.else_commands)
+
--- example.py
+++ example.py
@@ -4,57 +4,57 @@ from commanding.commands import *
 
 if __name__ == "__main__":
 
+   sysName = Run('uname -s')
+   isMac = sysName.matches("Darwin")
+   isLinux = sysName.matches("Linux")
+   pipFound = not Exists('pip').matches("not found")
+
    t = Task(
-         Do(
-            Where("uname -s").matches("Darwin"),
-            Do(
-               not os.path.exists("/Applications/iTerm 2.app"),
-               Task(
-                  Wget("http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip"),
-                  Unzip("iTerm2_v1_0_0.zip", "./tmp")
-               )
-            )
-         ),
-         Do(
-            Where("brew").matches("not found"),
-            Command('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"'),
-         ),
-         Do(
-            Where("pip").matches("not found"),
-            Task(
-               Do(
-                  Where("uname -s").matches("Darwin"),
-                  Task(
-                     ("pip")
-                  )
-               ),
-               Do(
-                  Where("uname -s").matches("Linux"),
-                  Apt("python-pip")
-               )
-            )
-         ),
-         Do(
-            Where("node").matches("not found"),
-            Task(
-               Do(
-                  Where("uname -s").matches("Darwin"),
-                  Brew("node")
-               ),
-               Do(
-                  Where("uname -s").matches("Linux"),
-                  Task(
-                     AptPPA("ppa:chris-lea/node.js"),
-                     AptUpdate(),
-                     Apt("nodejs")
-                  )
-               )
-            )
-         ),
-         Do(
-            Where("zsh").matches("not found"),
-            Command("curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh")
+
+      When(
+         isMac and not os.path.exists("/Applications/iTerm 2.app"),
+      ).do(
+         Wget("http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip"),
+         Unzip("iTerm2_v1_0_0.zip", "./tmp")
+      ),
+
+      When(
+         isMac and Exists("brew").matches("not found"),
+      ).do(
+         Run('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
+      ),
+
+      When(
+         not pipFound and isLinux,
+      ).do(
+         Apt("python-pip")
+      ).elseWhen(
+         not pipFound and isMac,
+      ).do(
+         Brew('pip')
+      ),
+
+      When(
+         Exists("node").matches("not found"),
+      ).do(
+            When(
+               isMac,
+            ).do(
+               Brew("node")
+            ).elseWhen(
+               isLinux,
+            ).do(
+               AptPPA("ppa:chris-lea/node.js"),
+               AptUpdate(),
+               Apt("nodejs")
          )
+      ),
+
+      When(
+         Exists("zsh").matches("not found"),
+      ).do(
+         Run("curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh")
       )
+   )
    t.run()
 
